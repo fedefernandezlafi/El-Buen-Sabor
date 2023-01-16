beb2327408ac9982f840b5f6c7c4331fcbafcfda
Federico Masnú <fmasnuprogram@gmail.com>
2023-01-16 15:14:33 -0300

    Controladores y  2 nuevos DTO

--- src/main/java/com/example/elbuensabor/Controladores/UsuarioControlador.java
+++ src/main/java/com/example/elbuensabor/Controladores/UsuarioControlador.java
@@ -1,20 +1,43 @@
 package com.example.elbuensabor.Controladores;
 
-import com.example.elbuensabor.Entidades.Usuario;
+import com.example.elbuensabor.DTOs.UsuarioDTO;
+import com.example.elbuensabor.Excepciones.ErrorServicio;
+import com.example.elbuensabor.Servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
 @RequestMapping ("/usuarios")
 public class UsuarioControlador {
+    @Autowired
+    private UsuarioServicio usuarioServicio;
 
 
 @GetMapping
-    public ResponseEntity<Usuario> listarUsuarios () {
-    return ResponseEntity.ok(new Usuario());
+    public ResponseEntity<List<UsuarioDTO> > listarUsuarios () {
+    return ResponseEntity.ok(usuarioServicio.listarUsuarios());
+
 }
+
+   @PostMapping("/registro")
+    public ResponseEntity<UsuarioDTO> registro (@Valid @RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(usuarioServicio.crearUsuario(usuarioDTO));
+   }
+
+   @PutMapping("/actualizar")
+    public ResponseEntity<UsuarioDTO> actualizar (@Valid @RequestBody UsuarioDTO usuarioDTO, @PathVariable long id) throws ErrorServicio {
+        if(usuarioServicio.actualizarUsuario(usuarioDTO, id) == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(usuarioServicio.actualizarUsuario(usuarioDTO,id));
+
+
+   }
+
 
 }
