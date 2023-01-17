90e492fb9bd4270af83f972094bf6ac1bd00ff6f
Federico Fernandez Lafi <ffernandezlafi@gmail.com>
2023-01-17 17:26:36 -0300

    agregando column en entidad factura

--- src/main/java/com/example/elbuensabor/Entidades/Factura.java
+++ src/main/java/com/example/elbuensabor/Entidades/Factura.java
@@ -19,17 +19,20 @@ public class Factura {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @CreationTimestamp
     @Column (name = "fecha", nullable = false)
     private LocalDate fecha;
     @Column (name = "numero", nullable = false)
     private int numero;
-    @Column (name = "monto_descuento")
+    @Column (name = "monto_descuento", nullable = false)
     private double montoDescuento;
+    @Column (name = "forma_pago", nullable = false)
     private String formaPago;
+    @Column (name = "nro_Tarjeta", nullable = false)
     private String nroTarjeta;
+    @Column (name = "total_Venta", nullable = false)
     private double totalVenta;
+    @Column (name = "total_Costo", nullable = false)
     private double totalCosto;
 
 }
