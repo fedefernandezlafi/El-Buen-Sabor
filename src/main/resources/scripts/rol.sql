ba606339291f799ddbb8ba08f746b48a747dcb0e
Federico Masnú <fmasnuprogram@gmail.com>
2023-02-10 18:27:39 -0300

    Agregando sql

---
+++ src/main/resources/scripts/rol.sql
@@ -0,0 +1,11 @@
+ USE ELBUENSABOR;
+ 
+ 
+ -- QUINTA INSERCION         
+ 
+ 
+ INSERT INTO ROL(ID,ROL_NOMBRE)
+      VALUES(1,"0"),
+            (2,"1"),
+            (3,"3"),
+            (4,"2");
